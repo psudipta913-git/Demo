453c2bdb72d02df3bd2a7e1642923e11baedf01a
psudipta913-git <psudipta913@gmail.com>
2021-10-25 20:04:44 +0530

    commiting to premaster branch

--- Demo/src/tests/demoTests.java
+++ Demo/src/tests/demoTests.java
@@ -4,7 +4,7 @@ public class demoTests {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("demo project XXX");
+		System.out.println("demo project XXX2");
 	}
 
 }
